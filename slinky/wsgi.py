83f5b5edd5a23bcde809bdd5165f1ec256675f38
dvejmz <dvejm@gmail.com>
2019-03-22 22:38:13 +0000

    Add basic /start endpoint

--- slinky/wsgi.py
+++ slinky/wsgi.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 application = Flask(__name__)
 
+questions = {
+    "1": {"title":"Demographics","hint":"AdditionalInformation","questions":[{"title":"Are you male or female?","hint":"This is a test radio hint","type":"radio","name":"gender","inline":True,"options":[{"id":1,"value":"male","hint":"Male Hint","text":"Male"},{"id":2,"hint":"Female hint","value":"female","text":"Female"},{"id":3,"hint":"Other hint","value":"other","text":"Other"}]}]}
+};
 
-@application.route("/question/<int:question_id>")
-def question(question_id: str):
-    return jsonify({
-        "answerIds": [1, 4],
-        "question": { "test": f"test {question_id}" }
-    })
+def get_question(id):
+    return questions[id]
+
+@application.route("/start")
+def start():
+    return jsonify(get_question("1"))
