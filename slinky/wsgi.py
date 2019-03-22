9054b09dbb50eeb14a03d99a6f8e36c914df17e4
Edward Hiley <edward.hiley@hscic.gov.uk>
2019-03-22 22:48:06 +0000

    first

--- slinky/wsgi.py
+++ slinky/wsgi.py
@@ -1,13 +1,27 @@
 from flask import Flask, jsonify
 application = Flask(__name__)
 
-questions = {
-    "1": {"title":"Demographics","hint":"AdditionalInformation","questions":[{"title":"Are you male or female?","hint":"This is a test radio hint","type":"radio","name":"gender","inline":True,"options":[{"id":1,"value":"male","hint":"Male Hint","text":"Male"},{"id":2,"hint":"Female hint","value":"female","text":"Female"},{"id":3,"hint":"Other hint","value":"other","text":"Other"}]}]}
-};
+db = dict()
 
-def get_question(id):
-    return questions[id]
 
-@application.route("/start")
+@application.route("/api/start")
 def start():
-    return jsonify(get_question("1"))
+    return jsonify(
+        {
+            "answerIds": [1],
+            "sessionId": "testSessionID",
+            "pageView": {
+                "title": "Demographics", "hint": "AdditionalInformation",
+                "questions": [
+                    {"title": "Are you male or female?",
+                     "hint": "This is a test radio hint",
+                     "type": "radio",
+                     "name": "gender",
+                     "inline": True,
+                     "options": [{"id": 1, "value": "male", "hint": "Male Hint", "text": "Male"},
+                                 {"id": 2, "hint": "Female hint", "value": "female", "text": "Female"},
+                                 {"id": 3, "hint": "Other hint", "value": "other", "text": "Other"}]}]}
+        }
+
+    )
+
