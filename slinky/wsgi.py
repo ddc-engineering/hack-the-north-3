fed89bd73e4210fbe7dc3110f9de20bc18776b91
Edward Hiley <edward.hiley@hscic.gov.uk>
2019-03-22 21:29:50 +0000

    basic question

--- slinky/wsgi.py
+++ slinky/wsgi.py
@@ -2,6 +2,9 @@ from flask import Flask
 application = Flask(__name__)
 
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+@application.route("/question/<int:question_id>")
+def question(question_id: str):
+    return {
+        "answerIds": [1, 4],
+        "question": { f"test {question_id}" }
+    }
